43e5494dffa9c377a253eb43cc45a7363baf9dbb
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-07 13:15:43 -0700

    commit update

--- first-console-App/Program.cs
+++ first-console-App/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace first_console_App
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
         }
     }
 }
